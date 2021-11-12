f26187cf33b0f2f491e2544e2042234f248567c0
srinadh1304 <srinadh1304@gmail.com>
2021-11-12 08:02:01 +0530

    ADDED: css for the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div classsName="App">
+      <Router>
+        <Switch>
+          <Route exact path="">
+            <PayrollForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
